7b07f3a045560c4d9da7df5a5778b23257e01cd6
Adrian L Lange <adrianlund@gmail.com>
2012-11-30 20:33:43 +0100

    Rework a little bit and change the portraits

--- Thaumaturge.lua
+++ Thaumaturge.lua
@@ -1,32 +1,31 @@
-local addonName = ...
-local Thaumaturge = CreateFrame('Frame', addonName)
 
-local function PreForge(self, id)
+local Thaumaturge = CreateFrame('Frame')
+
+local function Reforge(self, id)
 	ReforgeItem(id)
 end
 
 local info = {}
-local function InitDropdown()
-	wipe(info)
+function Thaumaturge:initialize()
+	table.wipe(info)
 
 	local currentID = GetReforgeItemInfo()
 	local sources = {GetSourceReforgeStats()}
-	for i = 1, #sources, 3 do
-		local srcName = sources[i]
-		local srcValue = sources[i + 2]
+	for source = 1, #sources, 3 do
+		local sourceName = sources[source]
+		local sourceValue = sources[source + 2]
 
-		local destinations = {GetDestinationReforgeStats(sources[i + 1], srcValue)}
-		for j = 1, #destinations, 4 do
-			local destName = destinations[j]
-			local destValue = destinations[j + 2]
-			local id = destinations[j + 3]
+		local destinations = {GetDestinationReforgeStats(sources[source + 1], sourceValue)}
+		for destination = 1, #destinations, 4 do
+			local destinationName = destinations[destination]
+			local destinationValue = destinations[destination + 2]
+			local newID = destinations[destination + 3]
 
-			info.text = string.format('|cffff0000-%d %s|r > |cff00ff00+%d %s|r', srcValue, srcName, destValue, destName)
-			info.checked = currentID == id
-			info.notCheckable = currentID ~= id
-			info.arg1 = id
-			info.func = PreForge
-			info.keepShownOnClick = true
+			info.text = string.format('|cffff0000-%d %s|r > |cff00ff00+%d %s|r', sourceValue, sourceName, destinationValue, destinationName)
+			info.checked = currentID == newID
+			info.notCheckable = currentID ~= newID
+			info.arg1 = newID
+			info.func = Reforge
 			UIDropDownMenu_AddButton(info)
 		end
 	end
@@ -39,76 +38,58 @@ local function InitDropdown()
 	end
 end
 
-local currentItem
-local function ClearAll()
+local function ClearReforge()
 	CloseDropDownMenus()
 	SetReforgeFromCursorItem()
 	ClearCursor()
-	currentItem = nil
 end
 
-local reforgable = {
-	ITEM_MOD_CRIT_RATING_SHORT = true,
-	ITEM_MOD_DODGE_RATING_SHORT = true,
-	ITEM_MOD_EXPERTISE_RATING_SHORT = true,
-	ITEM_MOD_HASTE_RATING_SHORT = true,
-	ITEM_MOD_HIT_RATING_SHORT = true,
-	ITEM_MOD_MASTERY_RATING_SHORT = true
-}
+local function OnEnterHook(self)
+	CloseDropDownMenus()
 
-local currentStats = {}
-local function ReforgeHook(self)
 	local hasItem = GameTooltip:SetInventoryItem('player', self:GetID())
 	if(not hasItem) then
-		return ClearAll()
+		return ClearReforge()
 	end
 
 	local name, link, _, level = GetItemInfo(GetInventoryItemLink('player', self:GetID()))
 	if(level < 200) then
-		return ClearAll()
+		return ClearReforge()
 	end
 
-	local _, _, reforging = GetReforgeItemInfo()
-	if(name == reforging or link == currentItem) then return end
+	local _, _, currentName = GetReforgeItemInfo()
+	if(currentName ~= name) then
+		PickupInventoryItem(self:GetID())
+		SetReforgeFromCursorItem()
 
-	wipe(currentStats)
-	GetItemStats(link, currentStats)
-
-	for stat, value in pairs(currentStats) do
-		if(reforgable[stat] and value > 0) then
-			PickupInventoryItem(self:GetID())
-			SetReforgeFromCursorItem()
-
-			currentItem = link
-			return ToggleDropDownMenu(1, nil, Thaumaturge, self, 40, 40)
+		if(GetReforgeItemInfo()) then
+			ToggleDropDownMenu(1, nil, Thaumaturge, self, 40, 40)
 		end
 	end
-
-	ClearAll()
 end
 
-local origSize
 local origEnter = PaperDollItemSlotButton_OnEnter
 local origLeave = UIDropDownMenu_OnHide
+
+local function OnLeaveHook(...)
+	origLeave(...)
+	ClearReforge()
+end
+
 function Thaumaturge:FORGE_MASTER_OPENED()
 	if(not PaperDollFrame:IsVisible()) then
 		ToggleCharacter('PaperDollFrame')
 	end
 
-	PaperDollItemSlotButton_OnEnter = ReforgeHook
-	UIDropDownMenu_OnHide = function(...)
-		origLeave(...)
-		ClearAll()
-	end
+	SetPortraitToTexture(CharacterFramePortrait, [=[Interface\Reforging\Reforge-Portrait]=])
 
-	self:RegisterEvent('FORGE_MASTER_ITEM_CHANGED')
-end
-
-function Thaumaturge:FORGE_MASTER_ITEM_CHANGED()
-	ClearAll()
+	PaperDollItemSlotButton_OnEnter = OnEnterHook
+	UIDropDownMenu_OnHide = OnLeaveHook
 end
 
 function Thaumaturge:FORGE_MASTER_CLOSED()
+	CharacterFrame_UpdatePortrait()
+
 	PaperDollItemSlotButton_OnEnter = origEnter
 	UIDropDownMenu_OnHide = origLeave
 end
@@ -119,7 +100,6 @@ function Thaumaturge:PLAYER_LOGIN()
 	self:RegisterEvent('ADDON_LOADED')
 
 	self.displayMode = 'MENU'
-	self.initialize = InitDropdown
 
 	PaperDollFrame:HookScript('OnHide', function()
 		if(ReforgingFrame and ReforgingFrame:IsShown()) then
